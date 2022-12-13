b4302b1816949ccdd135be74993038e5cd462564
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2022-12-13 17:28:15 +0100

    Ajustement

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -13,24 +13,25 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProfileController extends AbstractController
 {
 
-    #[Route('/profil/{id}', name: 'app_profile', requirements: ['id' => '\d+'])]
-    public function profile(int $id, MusicianRepository $musicianRepository): Response
+    #[Route('/profil', name: 'app_profile')]
+    public function profile( MusicianRepository $musicianRepository,UserInterface $user): Response
     {
 
         {
 
-            $musician = $musicianRepository->find($id);
+           $musician = $musicianRepository->findOneBy(['email'=> $user->getUserIdentifier()]);
 
 
-            // Si le musicien n'existe pas en base de données on retourne une erreur 404
-            if ($musician === null) {
-                throw $this->createNotFoundException();
-            }
+
+
+
+
 
             return $this->render('profile/index.html.twig', ['musician' => $musician]);
 
